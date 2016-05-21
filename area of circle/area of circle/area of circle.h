2ee0937c44db8275b073b298da75b0403f69d1fd
Yangguanghaitian <475806859@qq.com>
2016-05-21 21:10:08 +0800

    first area of circle

---
+++ area of circle/area of circle/area of circle.h
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+const float PI = 3.14;
+double S(double r);
+
+double S(double r)
+{
+	double s;
+	s = PI*r*r;
+	return(s);
+}
---
+++ area of circle/area of circle/main.cpp
@@ -0,0 +1,13 @@
+#include"area of circle.h"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "请输入圆的半径：";
+	double r;
+	cin >> r;
+	cout << "圆的面积为：" << S(r) << endl;
+	return 0;
+}
